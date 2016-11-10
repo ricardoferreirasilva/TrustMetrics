d319530728c408701931cc64cf470e29e224e4f5
Ricardo Ferreira da Silva <tansvanio@hotmail.com>
2016-11-10 19:41:45 +0000

    New Classes

---
+++ src/trustMetrics/ProjectNode.java
@@ -0,0 +1,8 @@
+package trustMetrics;
+
+/* This class will be an agent representing the nodes in the project.
+ * 
+ */
+public class ProjectNode {
+
+}
---
+++ src/trustMetrics/Task.java
@@ -0,0 +1,7 @@
+package trustMetrics;
+
+// This will be the actual task, associated with an edge between to Project Nodes.
+public class Task {
+	String name;
+
+}
