6f8ee688bb653492019a2393c1c39f5f0cdb5ff7
armysick <uh4v3b33nh4ck3d@gmail.com>
2016-11-16 12:53:00 +0000

    Working Label programatic addition

--- src/trustMetrics/TaskStyle.java
+++ src/trustMetrics/TaskStyle.java
@@ -1,30 +1,46 @@
 package trustMetrics;
 
-import java.awt.Color; 
-import java.awt.Paint; 
-import java.awt.Stroke; 
-import repast.simphony.visualizationOGL2D.DefaultStyleOGL2D;
-import saf.v3d.scene.Position; 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Paint;
+import java.awt.Stroke;
+import java.awt.geom.Rectangle2D;
 
-public class TaskStyle extends DefaultStyleOGL2D 
-{ 
-	 @Override
-      public Color getColor(Object o)
-      {
-          if (o instanceof Task)
-          {
-             Task a = (Task) o;
-              
-              return Color.RED;
-          }
-          else
-          {
-              return Color.BLACK;
-          }
-      }
-	 @Override
-	 public String getLabel(final Object agent) {
-		String str = "test";
-		return str;
-	 }
-} 
+import org.piccolo2d.nodes.PText;
+
+import repast.simphony.essentials.RepastEssentials;
+import repast.simphony.visualization.editedStyle.DefaultEditedStyleData2D;
+import repast.simphony.visualization.editedStyle.EditedStyleData;
+import repast.simphony.visualization.editedStyle.EditedStyleUtils;
+import repast.simphony.visualizationOGL2D.DefaultStyleOGL2D;
+import saf.v3d.scene.Position;
+
+public class TaskStyle extends DefaultStyleOGL2D {
+	private RepastEssentials re = new RepastEssentials();
+	EditedStyleData<Object> style = new DefaultEditedStyleData2D<Object>();
+	Font font;
+	@Override
+	public Color getColor(Object o) {
+		if (o instanceof Task) {
+			Task a = (Task) o;
+			return Color.RED;
+		} else {
+			return Color.BLACK;
+		}
+	}
+
+	
+	@Override
+	public Font getLabelFont(Object agent){
+		font = new Font(style.getLabelFontFamily(), 
+		        style.getLabelFontType(),
+		        style.getLabelFontSize());
+		return font;
+	}
+	@Override
+	public String getLabel(Object agent) {
+		style.setLabel("teste");
+		return EditedStyleUtils.getLabel(style, agent);
+	}
+	
+}
